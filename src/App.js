8bb55743e1ac8d44ecd26149399e92faf2704f80
islam kamel <islam.kamel@agr.svu.edu.eg>
2022-09-10 04:03:09 +0200

    Refactoring code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
 import Register from "./pages/register/Register";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavigationBar from "./components/navbar/Navbar.jsx";
 import Login from "./pages/login/Login.jsx";
 function App() {
-  const [number, setNumber] = useState(1);
-  useEffect(() => {
-    console.log(number);
-  });
   return (
     <Router>
       <NavigationBar />
