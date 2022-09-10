22b7180495078814398cb611f9d74352fe50f6d2
islam kamel <islam.kamel@agr.svu.edu.eg>
2022-09-10 04:04:31 +0200

    Refactoring code

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,7 +4,6 @@ import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import { Link } from "react-router-dom";
 import "./navbar.css";
 import CreateButton from "../Buttons/createButton/CreateButton";
