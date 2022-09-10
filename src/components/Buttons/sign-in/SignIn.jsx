4a309a8057536ec8850fe863d3a79a4a335b027e
islam kamel <islam.kamel@agr.svu.edu.eg>
2022-09-10 04:02:37 +0200

    Fix img warn

--- src/components/Buttons/sign-in/SignIn.jsx
+++ src/components/Buttons/sign-in/SignIn.jsx
@@ -6,7 +6,7 @@ const SignIn = ({ operation, website, img }) => {
       type="button"
       className="login-with-google-btn md:w-[90%] w-[60%] justify-center mx-auto mt-9"
     >
-      <img src={img} />
+      <img src={img} alt={`${website}-icon`} />
       {operation} with {website}
     </button>
   );
