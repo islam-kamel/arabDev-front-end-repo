f0a0dcf8d84ab34cd532b157bb4cc9ff9b50f7b4
islam kamel <islam.kamel@agr.svu.edu.eg>
2022-09-11 18:13:41 +0200

    Refactoring code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React from "react";
-import "./App.css";
-import Register from "pages/register/Register";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavigationBar from "components/navbar/Navbar.jsx";
-import Login from "pages/login/Login.jsx";
+import React from 'react'
+import './App.css'
+import Register from 'pages/register/Register'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import NavigationBar from 'components/navbar/Navbar.jsx'
+import Login from 'pages/login/Login.jsx'
+
 function App() {
-  return (
-    <Router>
-      <NavigationBar />
-      <Routes>
-        <Route path="/" />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <NavigationBar />
+            <Routes>
+                <Route path="/" />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route />
+            </Routes>
+        </Router>
+    )
 }
 
-export default App;
+export default App
