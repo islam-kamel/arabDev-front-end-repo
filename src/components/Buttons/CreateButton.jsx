aa7f1b416df9b794e4c31af1d1381d6cee970d15
islam kamel <islam.kamel@agr.svu.edu.eg>
2022-09-25 01:40:31 +0200

    Create Button Reuseable Component

--- src/components/Buttons/CreateButton.jsx
+++ src/components/Buttons/CreateButton.jsx
@@ -5,15 +5,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 export default class CreateButton extends React.Component {
   constructor(props) {
     super(props)
-    this.fontAwesomeIcon = null
-    this.withIcon = null
-    this.icon = null
-    this.type = false
-    this.className = null
-    this.text = null
-    this.alt = null
-    this.path = null
-    this.onClick = null
+    this.fontAwesomeIcon = this.props.fontAwesomeIcon
+    this.withIcon = this.props.withIcon
+    this.icon = this.props.icon
+    this.type = this.props.type
+    this.className = this.props.className
+    this.text = this.props.text
+    this.alt = this.props.alt
+    this.path = this.props.path
+    this.onClick = this.props.onClink
+    this.authorization = this.props.authorization
+    this.href = this.props.href
   }
 
   image() {
@@ -23,18 +25,19 @@ export default class CreateButton extends React.Component {
   button() {
     return (
       <button
-        type={this.props.type ? this.props.type : 'button'}
+        type={this.type ? this.type : 'button'}
         className={`px-3 py-2  text-bold transition all 0.5s text-[#3b49df] border-2 border-[#3b49df] hover:bg-[#3b49df] rounded-md hover:text-[white] md:w-[100%] ${this.props.className}`}
         onClick={this.props.onClick}
       >
-        {this.props.text}
-        {this.props.withIcon && this.props.fontAwesomeIcon ? (
-          <FontAwesomeIcon icon={this.props.icon} />
-        ) : this.props.path ? (
-          this.image()
+        {this.authorization ? (
+          <a href={this.href} target={'_self'} rel={'noopener'}>
+            {this.text}
+          </a>
         ) : (
-          ''
+          this.text
         )}
+
+        {this.withIcon && this.fontAwesomeIcon ? <FontAwesomeIcon icon={this.icon} /> : this.path ? this.image() : ''}
       </button>
     )
   }
