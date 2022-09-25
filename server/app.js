36b5aef4ee36637f36ad0c8acfd97c12bc127e5a
islam kamel <islam.kamel@agr.svu.edu.eg>
2022-09-25 03:47:39 +0200

    Fixbug Missing rate limiting

--- server/app.js
+++ server/app.js
@@ -7,10 +7,20 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const cors = require('cors')
+const rateLimit = require('express-rate-limit')
 
 const githubCallback = require('./routes/githubCallback')
 
 const app = express()
+
+app.use(
+  rateLimit({
+    windowMs: 12 * 60 * 60 * 1000, // 12 hour duration in milliseconds
+    max: 5,
+    message: 'You exceeded 100 requests in 12 hour limit!',
+    headers: true,
+  })
+)
 
 app.use(logger('dev'))
 app.use(express.json())
