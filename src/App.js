3db9304130fd2b3596c5a31be1a24e355e13ebfd
mohamed ibrahim <mohamedibrahim.fed@gmail.com>
2022-09-30 22:06:45 +0200

    add post functionalty

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css'
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import NavigationBar from './components/navbar/Navbar.jsx'
-import React, { useState, useContext } from 'react'
+import React, { useState } from 'react'
 
 import Register from './pages/register/Register'
-import { IsLoggedInContext } from './context/IsLoggedInContext'
 import Login from './pages/login/Login.jsx'
 import CreatePost from './pages/create-post/CreatePost'
 import Home from './pages/Home/Home.jsx'
@@ -12,8 +11,6 @@ import Profile from './pages/Profile/Profile'
 
 function App() {
   const [isRendered, setIsRendered] = useState(false)
-  const { isLoggedIn } = useContext(IsLoggedInContext)
-
   return (
     <Router>
       <NavigationBar />
@@ -21,19 +18,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} replace />
         <Route path="/login" element={<Login />} replace />
-        <Route
-          path="/new"
-          element={
-            isLoggedIn ? (
-              <CreatePost isRendered={isRendered} setIsRendered={setIsRendered} />
-            ) : (
-              <Navigate to="/login" replace />
-            )
-          }
-        />
-        <Route path="/profile" element={isLoggedIn ? <Profile /> : <Navigate to="/login" replace />} />
+        <Route path="/new" element={<CreatePost isRendered={isRendered} setIsRendered={setIsRendered} />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
-      {/* </userDataContext.Provider> */}
     </Router>
   )
 }
--- src/components/Buttons/CreateButton.jsx
+++ src/components/Buttons/CreateButton.jsx
@@ -2,45 +2,20 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-export default class CreateButton extends React.Component {
-  constructor(props) {
-    super(props)
-    this.fontAwesomeIcon = this.props.fontAwesomeIcon
-    this.icon = this.props.icon
-    this.type = this.props.type
-    this.className = this.props.className
-    this.text = this.props.text
-    this.alt = this.props.alt
-    this.onClick = this.props.onClink
-    this.href = this.props.href
-  }
-
-  image() {
-    return <img src={this.props.icon} alt={this.alt + '-icon'} width={'32px'} style={{ display: 'inline-block' }} />
-  }
-
-  hyperLink() {
-    return (
-      <a href={this.href} target={'_self'} rel={'noopener'}>
-        {this.text}
-      </a>
-    )
-  }
-
-  button() {
-    return (
-      <button
-        type={this.type ? this.type : 'button'}
-        className={`px-3 py-2  text-bold transition all 0.5s text-[#3b49df] border-2 border-[#3b49df] hover:bg-[#3b49df] rounded-md hover:text-[white] md:w-[100%] ${this.props.className}`}
-        onClick={this.props.onClick}
-      >
-        {this.href ? this.hyperLink() : this.text}
-        {this.icon && this.fontAwesomeIcon ? <FontAwesomeIcon icon={this.icon} /> : this.icon ? this.image() : null}
-      </button>
-    )
-  }
-
-  render() {
-    return this.button()
-  }
+function CreateButton({ fontAwesomeIcon, icon, text, type = 'button', className, clickFunction }) {
+  return (
+    <button
+      type={type}
+      className={`px-3 py-2 text-bold transition all 0.5s text-[#3b49df] border-2 border-[#3b49df] hover:bg-[#3b49df] rounded-md hover:text-[white] md:w-[100%] ${className}`}
+      onClick={clickFunction}
+    >
+      {text}
+      {icon && fontAwesomeIcon ? (
+        <FontAwesomeIcon icon={icon} />
+      ) : icon ? (
+        <img src={icon} width={'32px'} style={{ display: 'inline-block' }} />
+      ) : null}
+    </button>
+  )
 }
+export default CreateButton
