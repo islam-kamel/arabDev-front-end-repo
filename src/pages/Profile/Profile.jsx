686489c2d7cdfcaba27d553dcb783b7ea06b6ab0
mohamed ibrahim <mohamedibrahim.fed@gmail.com>
2022-10-02 00:35:21 +0200

    rounded profile img

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -28,7 +28,7 @@ const Profile = () => {
         <div className="mt-[35px] w-fit mx-auto">
           <img
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI9HluZ0LFmY4x3AsIe537Tr9AHWMCxhH96a381hNb&s"
-            className="rounded-full border-4 border-white max-w-[200px] max-h-[200px] m-0"
+            className="rounded-[100%] border-4 border-white w-[140px] h-[140px] m-0"
           />
         </div>
         <div className="flex justify-center flex-col">
