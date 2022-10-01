78cae09e513a1be6269aad4c8ea274a220018261
mohamed ibrahim <mohamedibrahim.fed@gmail.com>
2022-10-01 02:19:02 +0200

    fetch and show posts

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import IsLoggedInContextProvider from './context/IsLoggedInContext'
 import SavedPostsContextProvider from './context/SavedPostsContext'
 import UserDataContextProvider from './context/UserDataContext'
+import PostsContextProvider from './context/PostsContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
@@ -13,9 +14,11 @@ root.render(
   <React.StrictMode>
     <IsLoggedInContextProvider>
       <SavedPostsContextProvider>
-        <UserDataContextProvider>
-          <App />
-        </UserDataContextProvider>
+        <PostsContextProvider>
+          <UserDataContextProvider>
+            <App />
+          </UserDataContextProvider>
+        </PostsContextProvider>
       </SavedPostsContextProvider>
     </IsLoggedInContextProvider>
   </React.StrictMode>
